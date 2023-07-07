abfa6e5ce7f962f92a8ab0f530706862f137b379
iamrohitg <rohitghosh76@gmail.com>
2023-07-07 21:04:38 +0530

    second commit

--- src/components/FreeCourseCard.js
+++ src/components/FreeCourseCard.js
@@ -3,23 +3,29 @@ import React from "react";
 const FreeCourseCard = (props) => {
   const { imgUrl, title, students, rating } = props.item;
 
+  const handleFreeButtonClick = () => {
+    window.open("https://www.youtube.com/playlist?list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n");
+  };
+
   return (
     <div className="single__free__course">
       <div className="free__course__img mb-5">
         <img src={imgUrl} alt="" className="w-100" />
-        <button className="btn free__btn">Free</button>
+        <button className="btn free__btn" onClick={handleFreeButtonClick}>
+          Free
+        </button>
       </div>
 
       <div className="free__course__details">
         <h6>{title}</h6>
 
-        <div className=" d-flex align-items-center gap-5">
-          <span className=" d-flex align-items-center gap-2">
-            <i class="ri-user-line"></i> {students}k
+        <div className="d-flex align-items-center gap-5">
+          <span className="d-flex align-items-center gap-2">
+            <i className="ri-user-line"></i> {students}k
           </span>
 
-          <span className=" d-flex align-items-center gap-2">
-            <i class="ri-star-fill"></i> {rating}k
+          <span className="d-flex align-items-center gap-2">
+            <i className="ri-star-fill"></i> {rating}k
           </span>
         </div>
       </div>
